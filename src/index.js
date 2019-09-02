f5f97c58357f56253e423918dc2e9b2fb787c24d
Seungwoo321 <seungwoo321@gmail.com>
2019-09-02 20:03:48 +0900

    demo update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import VuePivottable from './Pivottable'
 import VuePivottableUi from './PivottableUi'
 
+const components = {
+  VuePivottable,
+  VuePivottableUi
+}
 // VuePivottable.install = function (Vue) {
 //   Vue.component(VuePivottable.name, VuePivottable)
+// }
+
+// VuePivottableUi.install = function (Vue) {
 //   Vue.component(VuePivottableUi.name, VuePivottableUi)
 // }
+
+export default (Vue) => {
+  for (const key in components) {
+    Vue.component(components[key].name, components[key])
+  }
+}
 
 if (typeof window !== 'undefined' && window.Vue) window.Vue.use(VuePivottable)
 
@@ -12,5 +25,3 @@ export {
   VuePivottable,
   VuePivottableUi
 }
-
-// export default VuePivottable
