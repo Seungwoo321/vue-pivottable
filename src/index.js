6880b0d9beaad7afff472c54757c879861402daf
swlee <swlee@mz.co.kr>
2019-09-02 20:46:43 +0900

    0.2.7 build

--- src/index.js
+++ src/index.js
@@ -5,23 +5,16 @@ const components = {
   VuePivottable,
   VuePivottableUi
 }
-// VuePivottable.install = function (Vue) {
-//   Vue.component(VuePivottable.name, VuePivottable)
-// }
 
-// VuePivottableUi.install = function (Vue) {
-//   Vue.component(VuePivottableUi.name, VuePivottableUi)
-// }
+export {
+  VuePivottable,
+  VuePivottableUi
+}
+
+if (typeof window !== 'undefined' && window.Vue) window.Vue.use(VuePivottable)
 
 export default (Vue) => {
   for (const key in components) {
     Vue.component(components[key].name, components[key])
   }
-}
-
-if (typeof window !== 'undefined' && window.Vue) window.Vue.use(VuePivottable)
-
-export {
-  VuePivottable,
-  VuePivottableUi
 }
