5c37482b775afa1a12245d6a2bc5dfead095fbee
Seungwoo321 <seungwoo321@gmail.com>
2019-12-13 00:05:48 +0900

    Remove config for serve command

--- vue.config.js
+++ vue.config.js
@@ -3,14 +3,5 @@ module.exports = {
     output: {
       libraryExport: 'default'
     }
-  },
-  indexPath: '/demo/',
-  pages: {
-    index: {
-      entry: 'demo/main.js',
-      template: 'demo/index.html',
-      filename: 'index.html',
-      title: 'Index Page'
-    }
   }
 }
