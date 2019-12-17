486daf1336dfdc427f90e2e4f148acf3b34f6848
swlee <swlee@mz.co.kr>
2019-12-17 22:27:00 +0900

    Update build config in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,17 @@
+const path = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
 module.exports = {
   configureWebpack: {
     output: {
       libraryExport: 'default'
-    }
+    },
+    plugins: [
+      new CopyWebpackPlugin([{
+        from: path.join(__dirname, 'src/assets'),
+        to: path.join(__dirname, 'dist'),
+        toType: 'dir'
+      }])
+    ]
   }
 }
