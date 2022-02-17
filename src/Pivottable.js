21d69c56de3202ea6c8a166e33efc9b1274be766
cbbdev <55853311+cbbdev@users.noreply.github.com>
2022-02-17 17:29:57 -0500

    Update Pivottable.js

--- src/Pivottable.js
+++ src/Pivottable.js
@@ -1,5 +1,5 @@
-import TableRenderer from './TableRenderer'
-import defaultProps from './helper/common'
+import TableRenderer from './TableRenderer';
+import defaultProps from './helper/common';
 export default {
   name: 'vue-pivottable',
   mixins: [
@@ -7,17 +7,16 @@ export default {
   ],
   computed: {
     rendererItems () {
-      return this.renderers || Object.assign({}, TableRenderer)
+      return this.renderers || Object.assign({}, TableRenderer);
     }
   },
   methods: {
     createPivottable (h) {
       const props = this.$props
       return h(this.rendererItems[this.rendererName], {
-        props: {
-          ...props,
-          localeStrings: props.locales[props.locale].localeStrings
-        }
+        props: Object.assign({},
+          props,
+          {localeStrings: props.locales[props.locale].localeStrings})
       })
     },
     createWrapperContainer (h) {
