6f220ecf056d1a25e8c00d2c18184c2d40044ef4
Renato <rgdiascardoso@gmail.com>
2022-04-06 12:35:20 +0100

    Improve linting and cleanup dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,17 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
+  extends: [
     'plugin:vue/essential',
-    '@vue/standard'
+    '@vue/standard',
+    'prettier',
+    'plugin:prettier/recommended',
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: '@babel/eslint-parser'
   }
 }
