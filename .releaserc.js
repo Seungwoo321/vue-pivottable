0ac653c4ae4bdbfbac5e87cf47fb18a3f256c693
Renato <rgdiascardoso@gmail.com>
2022-06-02 12:06:38 +0100

    buid: package to github

---
+++ .releaserc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    '@semantic-release/git',
+  ],
+  release: {
+    prepare: [
+      '@semantic-release/changelog',
+      [
+        '@semantic-release/git',
+        { message: 'chore(release): ${nextRelease.version} [skip ci]' },
+      ],
+    ],
+  },
+}
