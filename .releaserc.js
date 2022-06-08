6d3b652f25631006ee80a58d2e71ec5f96b6c32c
André Domingues <7469472+voxivoid@users.noreply.github.com>
2022-06-08 13:39:19 +0100

    build(LEV-216): generate types on build. add test github workflow (#4)
    * LEV-216: add types generation to build command
    
    * LEV-216: update actions to run tests
    
    * fix action location
    
    * ignore scripts
    
    * test commit lint
    
    * build(LEV-216): fix commit lint
    
    * build(LEV-216): fix commit lint

--- .releaserc.js
+++ .releaserc.js
@@ -1,17 +1,18 @@
 module.exports = {
-  plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/npm',
+ plugins: [
+  '@semantic-release/commit-analyzer',
+  '@semantic-release/release-notes-generator',
+  '@semantic-release/npm',
+  '@semantic-release/git'
+ ],
+ release: {
+  prepare: [
+   '@semantic-release/changelog',
+   [
     '@semantic-release/git',
-  ],
-  release: {
-    prepare: [
-      '@semantic-release/changelog',
-      [
-        '@semantic-release/git',
-        { message: 'chore(release): ${nextRelease.version} [skip ci]' },
-      ],
-    ],
-  },
+    // eslint-disable-next-line no-template-curly-in-string
+    { message: 'chore(release): ${nextRelease.version} [skip ci]' }
+   ]
+  ]
+ }
 }
